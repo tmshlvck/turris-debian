bc1891dfaa84012c9d3f7884d89ae54aba7a4cca
Tomas Hlavacek <tmshlvck@gmail.com>
2019-10-10 09:44:11 +0200

    Add Linaro compilers and few packages to buildroot
    Add Linaro armhf and aarch64 compilers to buildroot
    Add few needed packages for buildroot
    Add trusted console /dev/pts/* which is needed by Debian login after
    systemd-nspawn starts the system

--- runbuildroot.sh
+++ runbuildroot.sh
@@ -11,13 +11,19 @@ create_all () {
 	cat <<EOF
 #!/bin/bash
 apt-get -y update
-apt-get -y install debootstrap qemu-user qemu-user-static git devscripts u-boot-tools git libssl-dev libncurses-dev
+apt-get -y install debootstrap qemu-user qemu-user-static git devscripts u-boot-tools git libssl-dev libncurses-dev bison flex bc rsync
 
 wget -O /opt/gcc-linaro-7.4.1-2019.02-x86_64_arm-linux-gnueabihf.tar.xz https://releases.linaro.org/components/toolchain/binaries/7.4-2019.02/arm-linux-gnueabihf/gcc-linaro-7.4.1-2019.02-x86_64_arm-linux-gnueabihf.tar.xz
 
 wget -O /opt/gcc-linaro-7.4.1-2019.02-x86_64_aarch64-linux-gnu.tar.xz https://releases.linaro.org/components/toolchain/binaries/7.4-2019.02/aarch64-linux-gnu/gcc-linaro-7.4.1-2019.02-x86_64_aarch64-linux-gnu.tar.xz
 
+	cd /opt
+	tar xf /opt/gcc-linaro-7.4.1-2019.02-x86_64_arm-linux-gnueabihf.tar.xz
+	rm /opt/gcc-linaro-7.4.1-2019.02-x86_64_arm-linux-gnueabihf.tar.xz
+	tar xf /opt/gcc-linaro-7.4.1-2019.02-x86_64_aarch64-linux-gnu.tar.xz
+	rm /opt/gcc-linaro-7.4.1-2019.02-x86_64_aarch64-linux-gnu.tar.xz
 
+	echo 'PATH=$PATH:/opt/gcc-linaro-7.4.1-2019.02-x86_64_aarch64-linux-gnu/bin:/opt/gcc-linaro-7.4.1-2019.02-x86_64_arm-linux-gnueabihf/bin' >>/root/.bashrc
 EOF
 	} | sudo tee $ROOT/root/init.sh
 
@@ -26,6 +32,7 @@ EOF
 	sudo cp /etc/resolv.conf $ROOT/etc
 	sudo cp ~/.gitconfig $ROOT/root/
  	echo "turris-debian" | sudo tee $ROOT/etc/hostname
+ 	echo "pts/0" | sudo tee -a $ROOT/etc/securetty
 	sudo systemd-nspawn -D $ROOT /root/init.sh
 }
 
