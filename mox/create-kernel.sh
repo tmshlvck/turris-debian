1b40d11fdc79994000871ee3fe17a000f853dce9
Tomas Hlavacek <tmshlvck@gmail.com>
2020-01-30 13:09:16 +0100

    Rework kernel versioning for MOX
    Rework versioning - use -mox local version and remove custom number
    versioning. Use default make deb-pkg.

--- mox/create-kernel.sh
+++ mox/create-kernel.sh
@@ -3,7 +3,7 @@
 # by Tomas Hlavacek (tmshlvck@gmail.com)
 #
 # prerequisities - install packages:
-# apt-get install git devscripts kernel-package libssl-dev libncurses-dev
+# apt-get install git devscripts kernel-package libssl-dev libncurses-dev equivs
 #
 # Linaro GCC 7.3 & toolchain in /opt
 #
@@ -16,32 +16,17 @@
 export ARCH=arm64
 export CROSS_COMPILE=aarch64-linux-gnu-
 
-#if [ -d linux ]; then
-#  cd linux
-##  git checkout $KERNELBRANCH
-#  make distclean
-#else
-#  git clone $KERNELREPO linux
-#  cd linux
-#  git checkout $KERNELBRANCH
-#  make distclean
-#fi
-
-R=`ls linux-image-*.deb | cut -d"_" -f2 | awk '$0>x{x=$0};END{print x}'`
-if [ -z "${R}" ]; then
-	R=0
-fi
-R=$(($R + 1))
+cd kernel
 
 if [ -f linux/.config ] && [ -f linux/arch/arm64/configs/mox_defconfig ]; then
-	if ! diff files/mox_defconfig linux/.config >/dev/null; then
+	if ! diff ../files/mox_defconfig linux/.config >/dev/null; then
 		echo "The config in Kernel tree differs from files/mox_defconfig. Copy it or remove it:"
-		echo "  ---->   cp linux/.config files/mox_defconfig"
-		echo "  ---->   rm linux/.config"
+		echo "  ---->   cp kernel/linux/.config files/mox_defconfig"
+		echo "  ---->   rm kernel/linux/.config"
 		exit 0
 	fi
 fi
-cp files/mox_defconfig linux/arch/arm64/configs
+cp ../files/mox_defconfig linux/arch/arm64/configs
 cd linux
 make mox_defconfig
 
@@ -50,6 +35,44 @@ export DEB_HOST_ARCH=arm64
 TOOLCHAINDIR=`dirname $(which ${CROSS_COMPILE}gcc)`
 CROSS_PREFIX="${TOOLCHAINDIR}/${CROSS_COMPILE}"
 
-#make-kpkg --rootcmd fakeroot --arch arm64 --cross-compile $CROSS_PREFIX --revision=$R kernel_image kernel_headers
-make -j $(nproc) deb-pkg KDEB_PKGVERSION=${R}
+make -j $(nproc) deb-pkg LOCALVERSION=-mox
+
+# crete meta-package
+cd ..
+
+KIP=`ls linux-image-*_arm64.deb | grep -v -- "-dbg_" | sort --version-sort | tail -n1`
+HIP=`ls linux-headers-*_arm64.deb | sort --version-sort | tail -n1`
+KDIP=`ls linux-image-*-dbg_*_arm64.deb | sort --version-sort | tail -n1`
+
+if ! [ -f $KIP ]; then
+	        echo "Missing file $KIP . Exit."
+		exit -1
+fi
+
+KIPN=`echo $KIP | awk -F'_' '{print $1}'`
+HIPN=`echo $HIP | awk -F'_' '{print $1}'`
+KDIPN=`echo $KDIP | awk -F'_' '{print $1}'`
+KV=`echo $KIP | awk -F'_' '{print $2}'`
+
+#equivs-control linux-kernel-mox.cfg
+cat > linux-kernel-mox.cfg <<EOF
+# Source: <source package name; defaults to package name>
+Section: misc
+Priority: optional
+Homepage: https://github.com/tmshlvck/turris-debian
+Standards-Version: 3.9.2
+
+Package: linux-kernel-mox
+Version: $KV
+Maintainer: Tomas Hlavacek <tmshlvck@gmail.com>
+Depends: $KIPN(>=$KV),$HIPN(>=$KV)
+Recommends: $KDIPN(>=$KV),linux-libc-dev(>=$KV)
+# Suggests: <comma-separated list of packages>
+# Provides: <comma-separated list of packages>
+# Replaces: <comma-separated list of packages>
+Architecture: arm64
+Description: Turris MOX up-to-date kernel metapackage.
+ This metapackage points to the latest working kernel for the board.
+EOF
+equivs-build linux-kernel-mox.cfg
 
